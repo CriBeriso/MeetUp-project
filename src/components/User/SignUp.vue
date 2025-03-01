7c5d8c052d3fc4cd8e30e97a1f2d517dda37da77
CriBeriso <berith93@icloud.com>
2025-03-01 12:31:17 +0100

    add firebase and sign up form #4

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,5 +1,80 @@
 <template>
-  <div>
-    <p>The Sign Up Page</p>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" offset-sm="3">
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-btn type="submit">Sign up</v-btn>
+                  </v-col>
+                </v-row>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Password do not match' : ''
+    }
+  },
+  methods: {
+    onSignUp () {
+      // vuex
+      console.log({email: this.email, password: this.password, confirmPassword: this.confirmPassword})
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 /**
  * main.js
- *
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
-
-
 // Plugins
 import { registerPlugins } from '@/plugins'
 import store from './store'
 import router from './router'
 import App from './App.vue'
 import { createApp } from 'vue'
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBgC8C2GhirAGpjrP4nqssyXqlpwlD9Hw8',
+  authDomain: 'meetupproject-5e537.firebaseapp.com',
+  projectId: 'meetupproject-5e537',
+  storageBucket: 'meetupproject-5e537.firebasestorage.app',
+  messagingSenderId: '823958977209',
+  appId: '1:823958977209:web:8f0b08ca587d4d5e0c3968'
+};
+const firebase = initializeApp(firebaseConfig);
 const app = createApp(App)
 app.config.globalProperties.$filters = {
   dateFormat (value) {
